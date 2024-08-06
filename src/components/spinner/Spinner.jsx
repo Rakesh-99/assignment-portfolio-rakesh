24c3decb59af07f41c2c67bea9772a4acd51dad3
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-08-06 13:58:27 +0530

    chore: ui imporve

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -8,9 +8,10 @@ import './Spinner.css';
 const Spinner = () => {
     return (
         <>
-            <div className="loader">
-
+            <div className="min-h-screen w-full justify-center items-center">
+                <span className='loader'></span>
             </div>
+
         </>
     )
 }
